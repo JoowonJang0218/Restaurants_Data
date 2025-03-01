250bf3bf34e8c3ebdcbcdb3bfb3d4c0026547118
Joowon Jang <jw11480110@gmail.com>
2025-03-01 21:41:05 +0900

    auth2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const { Client } = require('pg');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
